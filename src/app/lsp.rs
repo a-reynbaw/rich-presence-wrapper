71f5d8e259edec9663ed97196689b0e90c3c62a9
a-reynbaw <ammarinakou@gmail.com>
2026-07-05 17:11:10 +0300

    works (except artwork)

--- src/app/lsp.rs
+++ src/app/lsp.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::path::Path;
 use std::process::ExitCode;
 use std::time::{Duration, Instant, SystemTime};
 
@@ -9,7 +10,7 @@ use tokio::sync::{Mutex, MutexGuard};
 use tower_lsp::{LanguageServer, LspService, Server, lsp_types::*};
 
 use crate::discord::*;
-use crate::util::SystemTimeExt;
+use crate::util::{SystemTimeExt, find_repo_root, get_vcs_branch, home_dir};
 
 const CLIENT_ID: &str = "1523025249845903410";
 
@@ -64,37 +65,71 @@ struct Document {
 }
 
 impl LspTask {
-    fn build_activity(
+    #[instrument(skip(self, state))]
+    async fn update_presence(
         &self,
-        state: &MutexGuard<'_, State>,
-        active_document: &Url,
-    ) -> Activity<'static> {
+        state: &mut MutexGuard<'_, State>,
+        active_document: Option<&Url>,
+    ) {
+        let now = Instant::now();
+
+        if state.last_update.is_some_and(
+            |x| now - x < Duration::from_secs(1), /* TODO: fetch interval from config file */
+        ) {
+            debug!("skip update");
+            return;
+        }
+
+        debug!("update");
+        state.last_update = Some(now);
+
         let State { ref start, .. } = **state;
 
         let mut activity = Activity::new()
-            .name("todo")
             .activity_type(ActivityType::Playing)
             .status_display_type(StatusDisplayType::Name)
             .timestamps(Timestamps::new().start(start.duration_since_epoch().as_secs() as i64))
             .party(Party::new().size([1, 1]));
 
-        activity = activity.details("details").state("state");
-        activity
-    }
-
-    #[instrument(skip(self, state))]
-    async fn update_presence(&self, state: &mut MutexGuard<'_, State>, active_document: &Url) {
-        let now = Instant::now();
-
-        if state.last_update.is_some_and(|x| now - x < Duration::from_secs(1) /* TODO: fetch interval from config file */) {
-            debug!("skip update");
-            return;
+        if let Some(ref client) = state.client {
+            activity = activity.name(client.clone());
         }
-        
 
-        debug!("update");
-        state.last_update = Some(now);
-        let activity = self.build_activity(state, active_document);
+        if let Some(active_document) = active_document {
+            let document_path = Path::new(active_document.path());
+
+            activity = activity.details(
+                None.or_else(|| {
+                    let repo = find_repo_root(document_path)?;
+                    let repo_name = repo.file_name()?;
+                    let relative_document_path =
+                        document_path.strip_prefix(repo).unwrap_or(document_path);
+
+                    Some(format!(
+                        "{}: {}",
+                        repo_name.display(),
+                        relative_document_path.display()
+                    ))
+                })
+                .or_else(|| {
+                    let home = home_dir()?;
+
+                    Some(match document_path.strip_prefix(home) {
+                        Ok(x) => format!("{}", Path::new("~").join(x).display()),
+                        Err(_) => format!("{}", document_path.display()),
+                    })
+                })
+                .unwrap_or_else(|| format!("{}", document_path.display())),
+            );
+
+            if let Some(branch) = get_vcs_branch(document_path.parent().unwrap_or(document_path))
+                .await
+                .ok()
+                .flatten()
+            {
+                activity = activity.state(branch);
+            }
+        }
 
         if let Err(e) = state
             .discord
@@ -147,15 +182,15 @@ impl LanguageServer for LspTask {
 
     async fn did_open(&self, params: DidOpenTextDocumentParams) {
         debug!("did_open(params={params:#?})");
+        let uri = params.text_document.uri;
 
         let mut state = self.state.lock().await;
 
-        state
-            .documents
-            .entry(params.text_document.uri)
-            .insert_entry(Document {
-                language: params.text_document.language_id,
-            });
+        state.documents.entry(uri.clone()).insert_entry(Document {
+            language: params.text_document.language_id,
+        });
+
+        self.update_presence(&mut state, Some(&uri)).await;
     }
 
     async fn hover(&self, params: HoverParams) -> tower_lsp::jsonrpc::Result<Option<Hover>> {
@@ -164,7 +199,7 @@ impl LanguageServer for LspTask {
         let mut state = self.state.lock().await;
         self.update_presence(
             &mut state,
-            &params.text_document_position_params.text_document.uri,
+            Some(&params.text_document_position_params.text_document.uri),
         )
         .await;
 
@@ -178,7 +213,7 @@ impl LanguageServer for LspTask {
         debug!("did_change(params={params:#?})");
 
         let mut state = self.state.lock().await;
-        self.update_presence(&mut state, &params.text_document.uri)
+        self.update_presence(&mut state, Some(&params.text_document.uri))
             .await;
     }
 
@@ -186,7 +221,7 @@ impl LanguageServer for LspTask {
         debug!("did_save(params={params:#?})");
 
         let mut state = self.state.lock().await;
-        self.update_presence(&mut state, &params.text_document.uri)
+        self.update_presence(&mut state, Some(&params.text_document.uri))
             .await;
     }
 
@@ -195,5 +230,6 @@ impl LanguageServer for LspTask {
 
         let mut state = self.state.lock().await;
         state.documents.remove(&params.text_document.uri);
+        self.update_presence(&mut state, None).await;
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -3,6 +3,7 @@
 use std::borrow::Cow;
 use std::cmp::min;
 use std::future::pending;
+use std::io;
 use std::path::{Path, PathBuf};
 use std::process::{ExitCode, ExitStatus};
 use std::sync::OnceLock;
@@ -10,6 +11,7 @@ use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use regex::Regex;
 use serde::Deserialize;
+use tokio::process::Command;
 use tokio::time::{Instant, sleep_until};
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -216,4 +218,29 @@ where
 {
     let pattern = Cow::<'de, str>::deserialize(deserializer)?;
     Regex::new(&pattern).map_err(serde::de::Error::custom)
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+pub fn find_repo_root(in_repo: &Path) -> Option<&Path> {
+    in_repo.ancestors().find(|p| p.join(".git").is_dir())
+}
+
+pub async fn get_vcs_branch(repo: &Path) -> io::Result<Option<String>> {
+    let output = Command::new("git")
+        .args(["rev-parse", "--abbrev-ref", "HEAD"])
+        .current_dir(repo)
+        .output()
+        .await?;
+
+    let stdout = str::from_utf8(&output.stdout)
+        .map_err(|_| io::Error::from(io::ErrorKind::InvalidData))?
+        .trim();
+
+    if stdout.is_empty() {
+        return Ok(None);
+    }
+
+    let branch = stdout.to_owned();
+    Ok(Some(branch))
 }
